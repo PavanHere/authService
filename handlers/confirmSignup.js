a8abe305c3afdb893ca83e4419a7402338598755
PavanHere <upparapawan02@gmail.com>
2025-08-14 22:29:27 -0400

    adding basic auth service

---
+++ handlers/confirmSignup.js
@@ -0,0 +1,37 @@
+const { CognitoIdentityProviderClient, SignUpCommand, ConfirmDeviceCommand, ConfirmSignUpCommand } = require('@aws-sdk/client-cognito-identity-provider');
+
+const client = new CognitoIdentityProviderClient({ region: 'us-east-1' });
+const CLIENT_ID = "6s8fd3t0asd6tsdl8n269hrm3a";
+exports.confirmSignup = async(event)=>{
+
+
+
+    
+    const{email,confirmationCode}=JSON.parse(event.body);
+
+    const params={
+        ClientId: CLIENT_ID,
+      Username: email,
+      ConfirmationCode: confirmationCode,
+    
+      
+    };
+
+    try {
+        const command=new ConfirmSignUpCommand(params);
+        await client.send(command);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: "User successfully confirmed" })
+          };
+    } catch (error) {
+        
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'User Confirmation failed', details: error.message })
+          };
+    }
+
+    
+}
---
+++ handlers/signUp.js
@@ -0,0 +1,33 @@
+const { CognitoIdentityProviderClient, SignUpCommand } = require('@aws-sdk/client-cognito-identity-provider');
+
+const client = new CognitoIdentityProviderClient({ region: 'us-east-1' });
+const CLIENT_ID = "6s8fd3t0asd6tsdl8n269hrm3a";
+
+module.exports.signUp = async (event) => {
+  try {
+    const { email, password, fullName } = JSON.parse(event.body);
+
+    const params = {
+      ClientId: CLIENT_ID,
+      Username: email,
+      Password: password,
+      UserAttributes: [
+        { Name: 'email', Value: email },
+        { Name: 'name', Value: fullName }
+      ]
+    };
+
+    const command = new SignUpCommand(params);
+    await client.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: "User successfully signed up" })
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Sign-up failed', details: error.message })
+    };
+  }
+};
